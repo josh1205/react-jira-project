56237d566a04126a1fc22083c4b2033da3899cf5
Joshua Lavieri <joshualavieri@Joshuas-MacBook-Pro.local>
2024-09-14 15:29:57 -0400

    Added user authentication in the backend

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+
+    end
+  end
+
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
