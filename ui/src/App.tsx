1ece68d32cae974c9b02385705fec4ba2edfba22
Joshua Lavieri <joshualavieri@Joshuas-MacBook-Pro.local>
2024-09-14 13:20:06 -0400

    Implemented shell of application

---
+++ ui/src/App.tsx
@@ -0,0 +1,15 @@
+// App.js
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+const App = () => {
+   return (
+      <>
+         <Routes>
+            <Route path="/" element={<Login />} />
+         </Routes>
+      </>
+   );
+};
+ 
+export default App;
